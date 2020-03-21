9aec86e7ae6cc655d4ba97686d25b3ba1460b16e
ExTeroos <62008116+ExTeroos@users.noreply.github.com>
2020-03-21 16:43:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,25 @@ from pygame.locals import *
 clock = pygame.time.Clock()
 pygame.init()
 
-# do zrobienia dolaczenie do tego bardziej rozbudowanej fizyki
-
 pygame.display.set_caption('My Game Window')
 
 moving_right = False
 moving_left = False
-player_rect = pygame.Rect(50,50, player_Right.get_width(), player_Right.get_height())
+air_timer = 0
+
+player_rect = pygame.Rect(320,256, player_Right.get_width(), player_Right.get_height())
+
+def load_map(path):
+    f = open(path + '.txt','r')
+    data = f.read()
+    f.close()
+    data = data.split('\n')
+    game_map = []
+    for row in data:
+        game_map.append(list(row))
+    return game_map
+
+game_map = load_map('map')
 
 def collision_test(rect,tiles):
     hit_list = []
@@ -44,38 +56,49 @@ def move(rect,movement,tiles):
 while True:
     display.blit(background, (0, 0))
 
+    scroll[0] += (player_rect.x-scroll[0]-306)/5
+    scroll[1] += (player_rect.y - scroll[1] - 256)/5
+
     tile_rects = []
     y = 0
     for layer in game_map:
         x = 0
         for tile in layer:
             if tile == '1':
-                display.blit(dirt, (x*dirt.get_width(), y*dirt.get_height()))
+                display.blit(dirt, (x*dirt.get_width()-scroll[0], y*dirt.get_height()-scroll[1]))
             if tile == '2':
-                display.blit(grass, (x*grass.get_width(), y*grass.get_height()))
+                display.blit(grass, (x*grass.get_width()-scroll[0], y*grass.get_height()-scroll[1]))
             if tile != '0':
                 tile_rects.append(pygame.Rect(x*16,y*16,16,16))
             x += 1
         y += 1
+
     player_movement = [0,0]
     if moving_right == True:
         player_movement[0] += 2
     if moving_left == True:
         player_movement[0] -= 2
-    player_movement[1] += player_y_momentum
-    player_y_momentum += 0.2
-    if player_y_momentum > 3:
-        player_y_momentum = 3
+
+    player_movement[1] += vertical_momentum
+    vertical_momentum += 0.2
+    if vertical_momentum > 8:
+        vertical_momentum = 8
 
     player_rect,collisions = move(player_rect, player_movement, tile_rects)
 
     if collisions['bottom'] == True:
         air_timer = 0
-        player_y_momentum = 0
+        vertical_momentum = 0
     else:
         air_timer += 1
 
-    display.blit(player_Right, (player_rect.x, player_rect.y))
+    if collisions['top'] == True:
+        vertical_momentum = 0
+
+    if air_timer > 100:
+        player_rect = pygame.Rect(320, 256, player_Right.get_width(), player_Right.get_height())
+
+    display.blit(player_Right, (player_rect.x-scroll[0], player_rect.y-scroll[1]))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -88,7 +111,7 @@ while True:
                 moving_left = True
             if event.key == K_SPACE:
                 if air_timer < 6:
-                    player_y_momentum = -5
+                    vertical_momentum = -5
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
@@ -101,3 +124,4 @@ while True:
     screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (0, 0))
     pygame.display.update()
     clock.tick(60)
+
