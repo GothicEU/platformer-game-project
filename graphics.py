5dcb5096dac7eb65ed46d02f53cc8db2f9fd6d83
ExTeroos <62008116+ExTeroos@users.noreply.github.com>
2020-04-04 22:40:30 +0200

    Delete graphics.py

--- graphics.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-player_Right = pygame.image.load('graphics/right.PNG')
-player_Left = pygame.image.load('graphics/left.PNG')
-background = pygame.image.load('graphics/background.PNG')
-grass = pygame.image.load('graphics/grass.PNG')
-dirt = pygame.image.load('graphics/dirt.PNG')
